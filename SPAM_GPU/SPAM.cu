94b5fff1f3df98378ce06b6aff8e43aefbc95ac5
scrashedward <scrashedward@gmail.com>
2016-10-07 01:15:24 +0800

    no message

---
+++ SPAM_GPU/SPAM.cu
@@ -0,0 +1,17 @@
+#include "cuda.h"
+#include "cuda_runtime.h"
+#include "cuda_runtime_api.h"
+#include "iostream"
+
+
+using namespace std;
+
+int main(int argc, char** argv){
+	// the input file name
+	char * input = argv[1];
+	// the minimun support in percentage
+	int minSupPer = atof(argv[2]);
+	
+
+
+}
